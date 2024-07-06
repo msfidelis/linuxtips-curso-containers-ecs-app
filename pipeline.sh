c6b4813020b49182f29c5fed3211fcb4c2587b4e
Matheus Fidelis <matheus@nanoshots.com.br>
2024-07-06 13:13:01 -0300

    Aula Pipelines: CI Terraform

--- pipeline.sh
+++ pipeline.sh
@@ -29,10 +29,10 @@ echo "TERRAFORM - CI"
 
 cd ../terraform
 
-terraform init -backend-config=environment/$BRANCH_NAME/backend.tfvars
-
 echo "TERRAFORM - FORMAT CHECK"
 terraform fmt --recursive --check
+
+terraform init -backend-config=environment/$BRANCH_NAME/backend.tfvars
 
 echo "TERRAFORM - VALIDATE"
 terraform validate
