a8a37a3de631bd485fc18af523cbd997204d5f8e
Matheus Fidelis <matheus@nanoshots.com.br>
2024-07-06 12:02:14 -0300

    TESTE v3

--- pipeline.sh
+++ pipeline.sh
@@ -92,17 +92,18 @@ docker push $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$REPOSITORY_NAME:$GIT_C
 
 # APPLY DO TERRAFORM - CD
 
-cd ../terraform
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
+cd ../terraform
 
 REPOSITORY_TAG=$AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$REPOSITORY_NAME:$GIT_COMMIT_HASH
 
 echo "DEPLOY - TERRAFORM INIT"
-terraform init -backend-config=environment/dev/backend.tfvars
+terraform init -backend-config=environment/$BRANCH_NAME/backend.tfvars
 
 
 echo "DEPLOY - TERRAFORM PLAN"
-terraform plan -var-file=environment/dev/terraform.tfvars -var container_image=$REPOSITORY_TAG
+terraform plan -var-file=environment/$BRANCH_NAME/terraform.tfvars -var container_image=$REPOSITORY_TAG
 
 echo "DEPLOY - TERRAFORM APPLY"
-terraform apply --auto-approve -var-file=environment/dev/terraform.tfvars -var container_image=$REPOSITORY_TAG
+terraform apply --auto-approve -var-file=environment/$BRANCH_NAME/terraform.tfvars -var container_image=$REPOSITORY_TAG
