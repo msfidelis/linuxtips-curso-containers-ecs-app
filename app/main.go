6571d9539cc223b7e6ad5175dd6868bdb95c535f
Matheus Fidelis <matheus@nanoshots.com.br>
2024-07-06 13:52:37 -0300

    Aula Pipeline: Deploy Terraform

--- app/main.go
+++ app/main.go
@@ -8,7 +8,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/version", func(c *fiber.Ctx) error {
-		return c.SendString("v5")
+		return c.SendString("v6")
 	})
 
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
